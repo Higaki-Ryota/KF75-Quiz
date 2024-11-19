2193fc9ae4a592fc51671a610a6d214a9d72e805
Higaki-Ryota <higaki-ryota589@g.ecc.u-tokyo.ac.jp>
2024-11-19 18:05:28 +0900

    [add] test.vue

---
+++ pages/test.vue
@@ -0,0 +1,97 @@
+<template>
+    <div>
+      <a-background />
+      <t-menu v-if="displayState === 'menu'" @select="onLevelSelected" :Levels=Levels></t-menu>
+      <t-question v-else-if="displayState === 'question'" :count="10000000" :correctIndex="quizData[quizLevel][quizIndex]" :quizIndex="quizIndex" :quizNumber=quizIndex :level="quizLevel" :correctNumber="correctCount" :answerState="answerState" :answerDisplay="answerDisplay" :answerIndex="quizIndex * 4 + quizData[quizLevel][quizIndex] + 1" :correctDisplay="correctDisplay" :incorrectDisplay="incorrectDisplay" @select="onSelected" @timeout="timeout" />
+      <t-result v-else-if="displayState === 'result'" :correct-count="correctCount" @select="playAgain" />
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+    import {ref} from 'vue'
+    import {getRandomArray} from "../composables/utils";
+    import {quizData} from "../composables/quizData";
+    import { initializeApp } from "firebase/app";
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+    import firebase from 'firebase/compat/app';
+    import 'firebase/compat/firestore';
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyD1LeqIBFxE6wU2gE2xDTeSV_O4xTQSazw",
+      authDomain: "kf75-8d355.firebaseapp.com",
+      projectId: "kf75-8d355",
+      storageBucket: "kf75-8d355.firebasestorage.app",
+      messagingSenderId: "385498511746",
+      appId: "1:385498511746:web:9fc5581c8479ac83abf79b"
+    };
+  
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  
+    type DisplayState = "question" | "result" | "menu";
+    const displayState = ref<DisplayState>("menu");
+  
+    const quizLevel= ref(0);
+    const Levels = [["初級", "（小中学生向け）", "制限時間：60秒"],["中級","（高校生向け）","制限時間：60秒"],["上級","（大学生向け）","制限時間：60秒"]];
+    const onLevelSelected = (level:number) => {
+      quizLevel.value = level;
+      displayState.value = "question";
+      questionTime.value = Date.now();
+    }
+  
+    const quizIndex = ref(0);
+    const correctCount = ref(0);
+    const incorrectCount = ref(0);
+    const answerState = ref<"正解！" | "不正解" >(null as any);
+  
+    const answerDisplay = ref(false);
+    const correctDisplay = ref(false);
+    const incorrectDisplay = ref(false);
+    const questionTime = ref(0);
+    const answerTime = ref(0);
+  
+    const onSelected = (isCorrect: boolean) => {
+      if (answerDisplay.value === false) {
+        answerTime.value = Date.now();
+        answerDisplay.value = true;
+        if (isCorrect) {
+          correctCount.value++;
+          answerState.value = "正解！";
+          correctDisplay.value = true;
+        } else {
+          incorrectCount.value++;
+          answerState.value = "不正解";
+          incorrectDisplay.value = true;
+        }
+        if (answerTime.value < questionTime.value + 2000) {
+          setTimeout(() => {
+            quizIndex.value++;
+            questionTime.value = Date.now();
+            answerDisplay.value = false;
+            correctDisplay.value = false;
+            incorrectDisplay.value = false;
+          }, 3000 + questionTime.value - answerTime.value);
+        } else {
+          setTimeout(() => {
+            quizIndex.value++;
+            questionTime.value = Date.now();
+            answerDisplay.value = false;
+            correctDisplay.value = false;
+            incorrectDisplay.value = false;
+          }, 1000);
+        };
+      };
+      if(quizIndex.value>quizData[quizLevel.value].length){
+        displayState.value = "result";
+      }
+    };
+  
+  
+    const playAgain = () => {
+      displayState.value = "menu";
+      quizIndex.value = 0;
+      correctCount.value = 0;
+      incorrectCount.value = 0;
+    };
+  </script>
